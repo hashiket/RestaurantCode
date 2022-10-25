3d7c40d594b498a01a0176240d50650941bcce1d
Hashiket <hashiketbhoyar@gmail.com>
2022-10-25 18:15:09 +0530

    second commit

--- programRestaurant.py
+++ programRestaurant.py
@@ -12,51 +12,44 @@ with open("ResLogFile.txt", 'r') as file1:
             LogData = json.loads(file1.read())
 
 
-start=1
-while(start):
-    print("!!!!!!!!WELCOME TO OUR RESTAURANT!!!!!!!!!!")
-   
-    print("1: Order Food")
-    print("2: Get the top 3 food")
-    print("3: Exit")
 
 
-    choice  = int(input("Please enter your Choice :"))
-
-    #here is also option of odering food items
-    if choice==1:    
-        print("List of Foods available..")
-        for key,value in data.items():
-            print(str(key)+": "+value)
-        name=input("Enter our Name")
-        food_choice = int(input("Enter your choice of food.."))
-        Eln = len(EaterData)+1
-        EaterData[Eln] = name
-        with open("Eater.txt", 'w') as file1:
-           file1.write(json.dumps(EaterData))
-
-        LogData[len(LogData)+1] = [Eln,food_choice]
-        with open("ResLogFile.txt", 'w') as file1:
-           file1.write(json.dumps(LogData))
-    #we can get top 3 ordered items..
-    elif choice==2:
-        print("!!!!!!!! Top 3 most famous Food Items are !!!!!!!!!")
-        dct = {}
-        for key,value in LogData.items():
-
-            if value[1] in dct:
-                dct[value[1]] += 1
-            else:
-                dct[value[1]] = 1
-        start=0
-        while(start!=3):
-            Keymax = max(zip(dct.values(), dct.keys()))[1]
-            start+=1
-            print(str(start)+": "+data[str(Keymax)])
-            dct.pop(Keymax)
-            
-    #if we want to exit
-    elif choice==3:
-        start=0
     
+stl = 0  
+while(stl!=1):
+
+    dct = {}
+
+
+    end = len(LogData)
+    st=0
+    count=0
+    for key,value in LogData.items():
+        second = value[0]
+        item=value[1]
+        for key,value in LogData.items():
+            
+            if value[0]==second and item==value[1]:
+                count+=1
+        if count>1:
+            print("Got the error as eater_id found with same food")
+            stl=1
+            break
+        count=0
+    if stl==1:
+        break
+    for key,value in LogData.items():    
+        if value[1] in dct:
+            dct[value[1]] += 1
+        else:
+            dct[value[1]] = 1
+    start=0
+    print("!!!!!!!! Top 3 most famous Food Items are !!!!!!!!!")
+    while(start!=3):
+        Keymax = max(zip(dct.values(), dct.keys()))[1]
+        start+=1
+        print(str(start)+": "+data[str(Keymax)])
+        dct.pop(Keymax)
+                
+    stl+=1
 
